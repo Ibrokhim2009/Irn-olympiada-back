0539de987343810a8566e5aaf6c2f87728278dd9
Shukhrat <shuhratshukrillaev123@gmail.com>
2026-05-17 00:05:05 +0500

    feat: initialize project URL configuration and add payment and API routes

--- src/urls.py
+++ src/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 from core.views import PaymeCallbackView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('payment/payme/', PaymeCallbackView.as_view(), name='payme-callback-root'),
 ]
